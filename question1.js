6f438fffa9f22c86ba99013eac58c80435d0431a
mbabaziviolet <mbabazijoy549@gmail.com>
2021-12-13 11:13:31 -0800

    testing

--- question1.js
+++ question1.js
@@ -4,52 +4,51 @@
     console.log(people[value])
  }
 
-// qn2
+// // qn2
  let people = ["Mariam", "Gorret", "Joy","Robinah", "Aladina","Bena"];
   let x =[0]
   people.splice("Mariam")
  console.log(x)
 
-//qn3
+// //qn3
 people.splice("Bena")
-//qn4
+// //qn4
 let display=document.getElementById("root")
 let people = ["Mariam", "Gorret", "Joy","Robinah", "Aladina","Bena"];
 people.push("Janet")
 display.innerHTML=people
 
-//qn5
+// //qn5
 people.push("Vivian")
 display.innerHTML=people
 
-//qn6
+// //qn6
 let people = ["Mariam", "Gorret", "Joy","Robinah", "Aladina","Bena"];
 console.log(people);
  for(let value in people){
     console.log(people[1]);
 }
 
-//qn7
+// //qn7
 
 let people = ["Mariam", "Gorret", "Joy","Robinah", "Aladina","Bena"];
 let people1=people.slice(16,43);
 console.log(people1);
 
-//qn8
-
+// //qn8
 let people = ["Mariam", "Gorret", "Joy","Robinah", "Aladina","Bena"];
+let person = people.indexOf("Gorret") + 0
+console.log(person);
+//qn9
+let people = ["Mariam", "Gorret", "Joy","Robinah", "Aladina","Bena","Foo"];
+let names = people.indexOf("Foo") +0
+console.log(names);
 
-let first=people.indexOf(myfunction)
-
-function myfunction(value,index,array) {
-     return value >1;
-    
-}
- console.log(myfunction());
-
-//qn10
+// //qn10
 let display=document.getElementById("root")
 let people = ["Mariam", "Gorret", "Joy","Robinah", "Aladina","Bena"];
 people.push("Elizabeth","Artie")
 people.splice("Aladina")
 display.innerHTML=people
+
+
